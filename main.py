944c1f1e6f5fb3af9ddd76960f0f59b029a9acc9
GilHeller <gil151200@gmail.com>
2025-04-26 11:42:44 +0300

    FIX: handle one angle only

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from matplotlib.axes import Axes
 import numpy as np
 from pathlib import Path
 import cv2
@@ -5,44 +6,9 @@ import matplotlib.pyplot as plt
 import os
 from dotenv import load_dotenv
 
-from clibration import KITTICalibration
+from calibration import KITTICalibration
 
 load_dotenv(dotenv_path=".env")  
-
-# if __name__ == "__main__": 
-#     KITTI_PATH = os.environ.get("KITTI_PATH")
-    
-#     file = "000000"
-#     clibration_file_path = Path(KITTI_PATH) / "calib" / f"{file}.txt"
-#     velodyne_path = Path(KITTI_PATH) / "velodyne" / f"{file}.bin"
-#     image_path = Path(KITTI_PATH) / "image_2" / f"{file}.png"
-    
-#     image = cv2.imread(image_path)
-#     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    
-#     lidar = np.fromfile(velodyne_path, dtype=np.float32).reshape(-1, 4)
-
-#     calibration_obj = KITTICalibration(clibration_file_path)
-#     img_points, depth  = calibration_obj.project_lidar_to_image(lidar_points=lidar)
-    
-    
-#     # todo: understand mask:
-#     mask = (depth > 0) & (img_points[:, 0] >= 0) & (img_points[:, 1] >= 0) & \
-#        (img_points[:, 0] < image.shape[1]) & (img_points[:, 1] < image.shape[0])
-       
-#     img_points = img_points[mask]
-#     depth = depth[mask]
-
-#     # === Visualize ===
-#     empty_image = np.zeros(image.shape[:2], dtype=np.uint8)
-    
-#     plt.figure(figsize=(12, 6))
-#     plt.imshow(empty_image)
-#     plt.scatter(img_points[:, 0], img_points[:, 1], c=depth, cmap='jet', s=1)
-#     plt.title("Projected LiDAR Points on Image")
-#     plt.axis('off')
-#     plt.colorbar(label='Depth (m)')
-#     plt.show()
 
 
 if __name__ == "__main__": 
@@ -61,9 +27,12 @@ if __name__ == "__main__":
     calibration_obj = KITTICalibration(clibration_file_path)
 
     # Rotations to visualize
-    angles = [-30, -15, 0, 15, 30]
+    angles = [0]
     fig, axes = plt.subplots(1, len(angles), figsize=(20, 5))
 
+    if len(angles) == 1:
+        axes = [axes]
+        
     for ax, angle_deg in zip(axes, angles):
         img_points, depth = calibration_obj.rotate_camera_and_project(lidar_points=lidar, angle_deg=angle_deg)
         # img_points, depth = calibration_obj.rotate_camera_vertically_and_project(lidar_points=lidar, angle_deg=angle_deg)
@@ -82,5 +51,6 @@ if __name__ == "__main__":
 
     fig.suptitle("Projected LiDAR Points on Different Rotated Camera Views", fontsize=16)
     plt.tight_layout()
-    cbar = fig.colorbar(scatter, ax=axes.ravel().tolist(), shrink=0.6, label='Depth (m)')
+    cbar = fig.colorbar(scatter, ax=axes, shrink=0.6, label='Depth (m)')
+
     plt.show()
