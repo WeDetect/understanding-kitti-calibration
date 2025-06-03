858427abcb73877cf56b9f429214b99a3801c0b1
GilHeller <gil151200@gmail.com>
2025-06-03 21:38:31 +0300

    FIX: package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
     
 setup(
-    name='point_cloud_handler',
+    name='point_cloud_handlers',
     version='0.1.1',
     packages=find_packages(), 
     install_requires=requirements,    
